9b45d46b6035d6cbd0e88c63392fc63e5845df98
Documenter.jl <documenter@juliadocs.github.io>
2022-11-29 11:12:17 +0000

    build based on d84ac95

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BouncingBall","category":"page"},{"location":"#BouncingBall","page":"Home","title":"BouncingBall","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BouncingBall.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BouncingBall]","category":"page"}]
+}
